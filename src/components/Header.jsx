14ba19c932c400cc5c5cb5f5f508e729294bc35d
dhirajudhani <dhirajudhani870@gmail.com>
2024-06-05 21:30:12 +0530

    made some changes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,67 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { IoIosArrowDown } from "react-icons/io";
-import { CiLocationOn } from "react-icons/ci";
 import logo from "../Images/logo.jpg";
+import toast from "react-hot-toast";
 import useOnlineStatus from "../utils/useOnlineStatus";
 
-
-
 const Header = () => {
-  let [btnNameReact,setBtnNameReact] = useState("Login");
-  let onlineStatus = useOnlineStatus()
+  let [btnNameReact, setBtnNameReact] = useState("Login");
+  const [showNavItems, setShowNavItems] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+  let onlineStatus = useOnlineStatus();
+  const [nearMe, setNearMe] = useState(false);
+
+  // const handleLocationNearMe = () => {
+  //   navigator.geolocation.getCurrentPosition((postion) => {
+  //     const {latitude, longitude} = postion?.coords;
+  //     setNearMe(true)
+  //   })
+  // }
+
+  const toggleNavItems = () => {
+    setShowNavItems(!showNavItems);
+  };
 
   return (
-    <div className="flex justify-between shadow-lg">
-      <div className="w-20">
-        <img className="rounded-full p-1 border-black" src={logo} alt="Logo" />
+    <div className="flex justify-between shadow-lg z-10 items-center p-3 fixed w-full">
+      <div className=" flex items-center gap-5">
+        <Link to="/">
+          <img
+            className="rounded-full w-20 border-black"
+            src={logo}
+            alt="Logo"
+          />
+        </Link>
+        {nearMe ? (
+            <div className="flex items-center justify-center hover:text-[#F86603] flex-col"
+              onClick={() => {
+                handleLocationDefault();
+                toast.success("Switched to Default Location Ahmedabad");
+              }}
+            >
+              <Link to={"/"}>
+                <button className="text-lg pb-1">Default Location</button>
+              </Link>
+              <p className="text-lg font-bold">Ahmedabad</p>
+            </div>
+          ) : (
+            <div className="flex items-center justify-center hover:text-[#F86603]  flex-col"
+              onClick={() => {
+                handleLocationNearMe();
+                toast.success(`Switched to Nearby Location`);
+              }}
+            >
+              <Link to={"/"}>
+                <button className="text-lg pb-1">📍Locate Me</button>
+              </Link>
+              <p className="text-lg font-bold">Ahmedabad</p>
+            </div>
+          )}
       </div>
       <div className="flex items-center">
         <ul className="flex p-4 m-4">
           <li className="px-4">Online Status : {onlineStatus ? "✅" : "🔴"}</li>
-          <li className="px-4">
-            <Link to="/grocery">Grocery</Link>
-          </li>
+
           <li className="px-4">
             <Link to="/">Home</Link>
           </li>
