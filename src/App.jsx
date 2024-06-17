7a082eddb35d44838d07a9a63fafe7689f8f2cd2
dhirajudhani <dhirajudhani870@gmail.com>
2024-06-17 20:13:18 +0530

    created app store application

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,37 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function App() {
   return (
-    <div className="">
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-        gutter={30}
-        containerClassName="notification-container"
-        toastOptions={{
-          className: "notification-toast",
-          duration: 1500,
-        }}
-      />
-      <Header />
-      {/* Apply styles to position Outlet below Header */}
-      <div className=" flex flex-grow  mt-36 mb-4 w-full">
-        <div className="container mx-auto flex justify-center items-center">
-          <Outlet />
+    <Provider store={appStore}>
+      <div className="">
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          gutter={30}
+          containerClassName="notification-container"
+          toastOptions={{
+            className: "notification-toast",
+            duration: 1500,
+          }}
+        />
+        <Header />
+        {/* Apply styles to position Outlet below Header */}
+        <div className=" flex flex-grow  mt-36 mb-4 w-full">
+          <div className="container mx-auto flex justify-center items-center">
+            <Outlet />
+          </div>
+        </div>
+        <div className=" flex flex-grow  w-full">
+          <div className="container mx-auto flex justify-center items-center">
+            <Footer />
+          </div>
         </div>
       </div>
-      <div className=" flex flex-grow  w-full">
-        <div className="container mx-auto flex justify-center items-center">
-          <Footer />
-        </div>
-      </div>
-    </div>
+    </Provider>
   );
 }
 
