3caa683459f6c086086c603b8cfba39a7a7f4a32
dhirajudhani <dhirajudhani870@gmail.com>
2024-06-17 13:34:33 +0530

    change the restaurant category component

--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from 'react'
+import ItemList from './ItemList'
 
 const RestaurantCategory = ({data, resInfo}) => {
     const [showItems, setShowItems] = useState(true)
+    const handleClick = () => {
+      setShowItems(!showItems)
+    }
   return (
-    <div>RestaurantCategory</div>
+    <div>
+      <div className='category-container'>
+        <div className='category-header' onClick={handleClick}>
+          <h3>{data.title} ({data.itemCards.length})</h3>
+          {showItems ? <i className={`fa-solid fa-chevron-up`}></i> : <i className={`fa-solid fa-chevron-down`}></i>}
+        </div>
+        {showItems && <ItemList resInfo={resInfo} items={data.itemCards} />}
+      </div>
+      <div className='menu-border'></div>
+    </div>
   )
 }
 
